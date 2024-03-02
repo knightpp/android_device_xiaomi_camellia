a434b39ba033c10f66c38e9362bdfbb9ef3dbc5e
TogoFire <togofire@mailfence.com>
2024-03-02 10:51:56 -0500

    camellia: introduce Viper4Android FX
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: sabrina010209 <mcpe11640@gmail.com>

--- lineage_camellia.mk
+++ lineage_camellia.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
+# Inherit from ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
